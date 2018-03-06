b496ad44aa1cecf316d6e8155ed647148782e67b
AKARI-I <i.akari.1122@gmail.com>
2018-03-06 16:25:00 +0900

    Add enemy2, enemy3

--- director.rb
+++ director.rb
@@ -1,5 +1,6 @@
 class Director
   def initialize
+    
     @char1 = Enemy.new(rand(100), rand(400)+100, "images/enemy.png")
     @char2 = Enemy.new(rand(200), rand(300)+200, "images/enemy2.png")
     @char3 = Enemy.new(rand(300), rand(200)+300, "images/enemy3.png")
