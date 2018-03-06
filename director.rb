e72307ef2fbfb643a5f72de5b1f7fe4627f0fe39
AKARI-I <i.akari.1122@gmail.com>
2018-03-06 16:19:30 +0900

    Add enemy2, enemy3, random_start

--- director.rb
+++ director.rb
@@ -1,12 +1,18 @@
 class Director
   def initialize
-    @char = Enemy.new(0, 300, "images/enemy.png")
+    @char1 = Enemy.new(rand(100), rand(400)+100, "images/enemy.png")
+    @char2 = Enemy.new(rand(200), rand(300)+200, "images/enemy2.png")
+    @char3 = Enemy.new(rand(300), rand(200)+300, "images/enemy3.png")
     @bg_img = Image.load("images/background.png")
   end
 
   def play
     Window.draw(0, 0, @bg_img)
-    @char.move
-    @char.draw
+    @char1.move
+    @char2.move
+    @char3.move
+    @char1.draw
+    @char2.draw
+    @char3.draw
   end
 end
