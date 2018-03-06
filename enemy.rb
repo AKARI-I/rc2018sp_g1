55341a1ba84282935367c3ec4d2bf404310cf0b9
AKARI-I <i.akari.1122@gmail.com>
2018-03-06 17:46:17 +0900

    add basefiles

---
+++ enemy.rb
@@ -0,0 +1,17 @@
+class Enemy
+  def initialize(x, y, image_file)
+    @x, @y = x, y
+    @image = Image.load(image_file)
+    @image.set_color_key([0, 0, 0])
+    @dy = 3
+  end
+
+  def move
+    @y += @dy
+    @dy = -@dy if @y > 450 || @y < 0
+  end
+
+  def draw
+    Window.draw(@x, @y, @image)
+  end
+end
---
+++ main.rb
@@ -0,0 +1,21 @@
+require 'dxruby'
+
+require_relative 'director'
+require_relative 'enemy'
+require_relative 'enemy1'
+require_relative 'enemy2'
+require_relative 'enemy3'
+require_relative 'enemy4'
+require_relative 'enemy5'
+
+Window.caption = "RubyCamp2018"
+Window.width   = 600
+Window.height  = 800
+
+director = Director.new
+
+Window.loop do
+  break if Input.keyPush?(K_ESCAPE)
+
+  director.play
+end
