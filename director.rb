938e9f9bfc1a2f0352ad6fdf50ddbaf359ac5657
AKARI-I <i.akari.1122@gmail.com>
2018-03-06 17:49:15 +0900

    fix syntax error

--- director.rb
+++ director.rb
@@ -1,18 +1,12 @@
 class Director
   def initialize
-<<<<<<< HEAD
+
     #@enemy_x = [""]
     @enemy1 = Enemy1.new(50, 0, "images/enemy.png")
     @enemy2 = Enemy2.new(150, 0, "images/enemy2.png")
     @enemy3 = Enemy3.new(250, 0, "images/enemy3.png")
     @enemy4 = Enemy4.new(350, 0, "images/enemy4.png")
     @enemy5 = Enemy5.new(450, 0, "images/enemy5.png")
-=======
-    
-    @char1 = Enemy.new(rand(100), rand(400)+100, "images/enemy.png")
-    @char2 = Enemy.new(rand(200), rand(300)+200, "images/enemy2.png")
-    @char3 = Enemy.new(rand(300), rand(200)+300, "images/enemy3.png")
->>>>>>> b496ad44aa1cecf316d6e8155ed647148782e67b
     @bg_img = Image.load("images/background.png")
   end
 
