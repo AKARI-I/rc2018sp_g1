482fbf05716a371185d415f67674943b0e940cd9
sik103 <dongsiku+github@gmail.com>
2018-03-07 13:46:40 +0900

    Add enemy to player

--- gamescene/gamescene.rb
+++ gamescene/gamescene.rb
@@ -1,50 +1,63 @@
 require_relative 'player'
+require_relative 'director'
+# require_relative 'enemy'
 # require 'smalrubot'
 
-class Director
-    def initialize(board)
-        haba = 100
-        @player = Player.new(haba)
-        @board = board 
-        # @board = Smalrubot::Board.new(Smalrubot::TxRx::Serial.new)
-        # 上記の書き方だと、2回"board"が初期化されてしまう
-        # @board = board
-        @frm = 1
-        @dx = 0
-        @button = 0
-    end
-
-    def play
-        @player.draw_player
-        @player.draw_ball
-        if @frm == 30
-            @dx = @board.digital_read(4) 
-            @button = @board.digital_read(3)
-            p "katamuki:", @dx
-            p "button:", @button
-        end
-        @frm += 1
-        @frm = 1 if @frm > 30
-
-        if @dx > 0
-            @player.plus_player
-        else
-            @player.minus_player
+module Game
+    class Director
+        def initialize(board)
+            haba = 100
+            @player = Player.new(haba)
+            @board = board 
+            # @board = Smalrubot::Board.new(Smalrubot::TxRx::Serial.new)
+            # 上記の書き方だと、2回"board"が初期化されてしまう
+            # @board = board
+            @frm = 1
+            @dx = 0
+            @button = 0
+            
+            ## Enemy ##
+            @enemy = EnemyDirector::Director.new
+            ## end ##
         end
 
-        if @button > 0
-            @player.push_button = 1
-        elsif
-            @player.push_button = 0
+        def play
+            @player.draw_player
+            @player.draw_ball
+
+            ## Enemy ##
+            @enemy.play
+            ## end ##
+
+            if @frm == 30
+                @dx = @board.digital_read(4) 
+                @button = @board.digital_read(3)
+                p "katamuki:", @dx
+                p "button:", @button
+            end
+            @frm += 1
+            @frm = 1 if @frm > 30
+
+            if @dx > 0
+                @player.plus_player
+            else
+                @player.minus_player
+            end
+
+            if @button > 0
+                @player.push_button = 1
+            elsif
+                @player.push_button = 0
+            end
+
+
+
+            # if @score > 100
+            #     Scene.move_to(:happy_ending)
+                # エンディングに切り替わる
+            # else
+            #     Scene.move_to(:bad_endding)
+            # end
         end
-
-
-
-        # if @score > 100
-        #     Scene.move_to(:happy_ending)
-            # エンディングに切り替わる
-        # else
-        #     Scene.move_to(:bad_endding)
-        # end
     end
 end
