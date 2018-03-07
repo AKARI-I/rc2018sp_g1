6564521e3a1d194391b5f859afc95f4fe37dba4c
Vqlu3 <yamaken01.15.4383@gmail.com>
2018-03-07 13:11:36 +0900

    add file of main.rb and title.rb

---
+++ main.rb
@@ -0,0 +1,27 @@
+require 'dxruby'
+require 'smalrubot'
+# require_relative 'player'
+# require_relative 'scene'
+# require_relative 'scene_game\director'
+# require_relative 'scene_title\director'
+require_relative 'scenetitle\title'
+
+
+Window.width = 700
+Window.height = 600
+
+board = Smalrubot::Board.new(Smalrubot::TxRx::Serial.new)
+
+# Scene.add(Title::Director.new, :title)
+# Scene.add(Game1::Director.new(board), :game1)
+# Scene.add(Game2::Director.new(board), :game2)
+
+# Scene.move_to(:title)
+
+Scene = Title::Director.new(board)
+
+Window.loop do
+  break if Input.key_push?(K_ESCAPE)
+  # ゲームを終了させる
+  Scene.play
+end
--- scenetitle/title.rb
+++ scenetitle/title.rb
@@ -1,21 +1,16 @@
-
-require 'dxruby'
-
-Window.width = 800
-Window.height = 600
-
-
 module Title
     class Director
-board = Smalrubot::Board.new(Smalrubot::TxRx::Serial.new)
-        def initialize
+#board = Smalrubot::Board.new(Smalrubot::TxRx::Serial.new)
+        def initialize(board)
+        	@board = board
             @font = Font.new(32, "ＭＳ　Pゴシック")
+            @frm = 0
         end
 
         def play
             Window.draw_font(250, 280, "タイトル画面", @font)
             
-            @bc = @board.degital_read(3) if @frm == 30
+            @bc = @board.digital_read(3) if @frm == 30
             @frm += 1
             @frm=0 if @frm >30
             
