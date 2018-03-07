aa5a3d995ae2cddd08341ed8f53b7d383a17e39f
Ru6y <kazu22.grorydays@gmail.com>
2018-03-07 14:10:14 +0900

    Change xy of food.rb

--- food.rb
+++ food.rb
@@ -1,23 +1,27 @@
 require 'dxruby'
 
+#class Food
+	#@banana
+	#def damage
+		#@banana -= 1
+	#end
+#end
+
 bg = Image.load('background5.jpg', nil, nil, 600, 700)
-banana = Image.load('fruit_banana2.png')
+banana = Image.load('fruit_banana2_2.png')
 
 
 Window.width = 600
 Window.height = 700
 
-
-
-
 Window.loop do
-	@banana = banana
-	Window.draw(0, 0, bg)
-	Window.draw(0, 460, @banana)
-	Window.draw(100, 460, @banana)
-	Window.draw(200, 460, @banana)
-	Window.draw(300, 460, @banana)
-	Window.draw(400, 460, @banana)
-	break if Input.key_push?(K_ESCAPE)
-    
+	Window.draw(  0,   0, bg)
+	Window.draw(  0, 625, banana)
+	Window.draw(100, 625, banana)
+	Window.draw(200, 625, banana)
+	Window.draw(300, 625, banana)
+	Window.draw(400, 625, banana)
 end
+
+
+#fd = Food.new
