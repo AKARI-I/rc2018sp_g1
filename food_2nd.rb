2dd0178c434c85f85db2a4f809247092b5aadb57
Ru6y <kazu22.grorydays@gmail.com>
2018-03-07 11:48:29 +0900

    Add no backgraund images

---
+++ food_2nd.rb
@@ -0,0 +1,17 @@
+﻿require 'dxruby'
+
+bg = Image.load('background5.jpg', nil, nil, 600, 700)
+banana = Image.load('fluit_banana2.png')
+
+Window.width = 600
+Window.height = 700
+
+Window.loop do
+  	Window.draw(0, 0, bg)
+	Window.draw(0, 460, banana)
+	Window.draw(100, 460, banana)
+	Window.draw(200, 460, banana)
+	Window.draw(300, 460, banana)
+	Window.draw(400, 460, banana)
+	break if Input.key_push?(K_ESCAPE)
+end
