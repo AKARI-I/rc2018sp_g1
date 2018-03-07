0c435b6832951975fd7473531954568d8a2b389b
Vqlu3 <yamaken01.15.4383@gmail.com>
2018-03-07 23:18:20 +0900

    change ending design

--- endingscene/director.rb
+++ endingscene/director.rb
@@ -3,8 +3,9 @@ module NomalEnding
         # attr_accessor :pnt
 
         def initialize
-            @font = Font.new(32, "ＭＳ　Pゴシック")
-            @image = Image.load('images/test_bg.jpg')
+            @font = Font.new(32, "ＭＳ Pゴシック")
+            @font_e = Font.new(40, "ＭＳ Pゴシック")
+            @image = Image.load('images/ending_background3.jpg')
             # @pnt = 0
         end
 
@@ -12,8 +13,9 @@ module NomalEnding
             # @pnt = score
             Window.draw(0, 0, @image)
 
-            Window.draw_font(100, 280, "Successfully Completed", @font)
-            Window.draw_font(200, 400, "Your Score: #{format("%4d", score)}", @font)
+            Window.draw_font_ex(100, 280, "Successfully Completed", @font, {:edge_color => [0,0,0], :edge => true, :edge_width => 10, :edge_level => 9})
+            Window.draw_font_ex(200, 400, "Your Score:", @font, {:edge_color => [0,0,0], :edge => true, :edge_width => 10, :edge_level => 9})
+            Window.draw_font_ex(380, 400, "#{format("%4d", score)}", @font_e, {:edge_color => [255,0,0], :edge => true, :edge_width => 10, :edge_level => 9})
 
             return score
         end
